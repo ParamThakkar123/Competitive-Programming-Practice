32a10b35405da44bce2cff5e8e990be238db2d57
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-05-23 19:35:46 +0530

    Create CoinsAndTriangle.cpp
    This problem uses a very simple knowledge of mathematics and quadratic equations

---
+++ CoinsAndTriangle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T, N;
+	cin >> T;
+	while(T--){
+	    cin >> N;
+	    int n;
+	    n = (-1 + sqrt(1+(8*N)))/2;
+	    cout << n << endl;
+	}
+	return 0;
+}

cf45366c601d87476009cf6a840598bde8e65c72
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2024-01-17 20:53:33 +0530

    Create A - Christmas Present.cpp

---
+++ AtCoder/UNIQUE VISION Programming Contest 2023 Christmas (AtCoder Beginner Contest 334)/A - Christmas Present.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int b, g;
+    cin >> b >> g;
+
+    if(b > g){
+        cout << "Bat" << endl;
+    }
+    else{
+        cout << "Glove" << endl;
+    }
+}
